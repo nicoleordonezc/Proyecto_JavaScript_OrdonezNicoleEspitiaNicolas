e6c3dc76d85021ca5af69f8daf4d7107ba69dec8
Nicoleordonezc <nicoloe24@gmail.com>
2025-04-21 18:49:20 -0500

    feat: :lipstick: JS initiated

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { raza, clase, armadura, habilidades } from "./personajes.js"
+
+export const url = new URL("https://www.dnd5eapi.co/api/2014/") 
+
+
+raza();
+clase();
+armadura();
+habilidades();
---
+++ src/personajes.js
@@ -0,0 +1,32 @@
+import {url} from "./main.js"
+
+export const raza = async()=> {
+    url.pathname += "races"
+    const race =  await fetch(url.toString(), {method: "get"})
+    return await race.json();
+    
+}
+
+export const clase = async()=>{
+    const nuevo = new URL("https://www.dnd5eapi.co/api/2014/");
+    nuevo.pathname += "classes"
+    const clases = await fetch(nuevo.toString(), {method: "get"})
+    return await clases.json();
+    
+}
+
+export const armadura = async()=>{
+    const nuevo = new URL("https://www.dnd5eapi.co/api/2014/");
+    nuevo.pathname += "equipment-categories/weapon"
+    const armas = await fetch(nuevo.toString(), {method: "get"})
+    return await armas.json();
+    
+}
+
+export const habilidades = async()=>{
+    const nuevo = new URL("https://www.dnd5eapi.co/api/2014/");
+    nuevo.pathname += "skills"
+    const hEspeciales = await fetch(nuevo.toString(), {method: "get"})
+    return await hEspeciales.json();
+    
+}
